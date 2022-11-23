2c4eb552ce03d123f2a3e1af563894dbd76fa510
Ivisonj <ivison_j@outlook.com>
2022-11-22 21:14:49 -0300

    Primeira etapa concluída

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,7 +2,7 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
@@ -18,18 +18,18 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-            .then((response) => response.json())
-            .then(convertPokeApiDetailToPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 8) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
-            .then((response) => response.json())
-            .then((jsonBody) => jsonBody.results)
-            .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-            .then((detailRequests) => Promise.all(detailRequests))
-            .then((pokemonDetails) => pokemonDetails)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
 }
 
